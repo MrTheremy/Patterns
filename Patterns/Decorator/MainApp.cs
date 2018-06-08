4b151431023f71c456077975e81c02509662ecd9
Jolan Eysackers <jolaneysackers@hotmail.com>
2018-06-08 18:09:08 +0200

    composite

---
+++ Patterns/Decorator/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// SOURCE: http://www.dofactory.com/net/decorator-design-pattern
+/// TYPE: Structural Patterns
+/// DEFINITION: Attach additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
+/// </summary>
+
+namespace Decorator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
